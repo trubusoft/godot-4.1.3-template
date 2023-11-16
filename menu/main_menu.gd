dbfc848aa2ef6a05c9a67655ea5882dbf799e58d
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-11-16 15:24:08 +0700

    Connect Settings button from main_menu.tscn to settings.tscn

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -1,15 +1,14 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_how_to_play_button_pressed():
 	get_tree().change_scene_to_file("res://menu/how_to_play.tscn")
+
+func _on_setting_button_pressed():
+	get_tree().change_scene_to_file("res://menu/settings.tscn")
