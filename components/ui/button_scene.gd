26d1ee126e90eacd459ea62b4b8c765d4972eafa
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-11-17 09:16:11 +0700

    Make ButtonScene to call GlobalAudioPlayer for playing the click sound

--- components/ui/button_scene.gd
+++ components/ui/button_scene.gd
@@ -1,5 +1,5 @@
 # Given a `target_scene`, this component will change scene to it when being clicked
-
+# Supply it as a string formatted path to the target scene, for example: res://menu/main_menu.tscn
 
 extends Button
 
@@ -14,4 +14,5 @@ func _ready():
 
 func _on_pressed():
 	if target_scene != '':
+		GlobalAudioPlayer.play_mouse_click_sound()
 		get_tree().change_scene_to_file(target_scene)
